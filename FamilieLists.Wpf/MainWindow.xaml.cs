48c01d309b8f71397619bd77d37637d0947bfffc
Jens Debruyne <jens.debruyne@live.be>
2019-11-06 20:53:32 +0100

    Add instance familieLeden

--- FamilieLists.Wpf/MainWindow.xaml.cs
+++ FamilieLists.Wpf/MainWindow.xaml.cs
@@ -8,9 +8,24 @@ namespace FamilieLists.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> familieLeden = new List<string>()
+            {
+            "Jan|Mijne Man", "Jeanien|Mijn Trien", "Sid|My Kid"
+            };
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void btnVoegFamilielidToe_Click(object sender, RoutedEventArgs e)
+        {
+            string familielid;
+            string achternaam = txtAchternaam.Text;
+            string voornaam = txtVoornaam.Text;
+            familielid = voornaam + "|" + achternaam;
+            List<string> familieleden = new List<string>
+
         }
     }
 }
